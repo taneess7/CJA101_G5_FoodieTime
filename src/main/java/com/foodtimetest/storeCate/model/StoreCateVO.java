819af790016e3d1eb7bd4103d9076f5c89050ba1
Jamie Wu <meigo1229@gmail.com>
2025-06-01 23:03:45 +0800

    fix: 轉移 storeCate model

---
+++ src/main/java/com/foodtimetest/storeCate/model/StoreCateVO.java
@@ -0,0 +1,33 @@
+package com.foodtimetest.storeCate.model;
+
+import java.io.Serializable;
+
+public class StoreCateVO implements Serializable {
+
+	// 寫上所有欄位
+	private Integer storCateId; // 店家分類編號.
+	private String storCat; // 類型名稱
+
+	// 取得or設置
+
+	public StoreCateVO() {
+		super();
+	}
+
+	public Integer getStorCateId() {
+		return storCateId;
+	}
+
+	public void setStorCateId(Integer storCateId) {
+		this.storCateId = storCateId;
+	}
+
+	public String getStorCat() {
+		return storCat;
+	}
+
+	public void setStorCat(String storCat) {
+		this.storCat = storCat;
+	}
+
+}
