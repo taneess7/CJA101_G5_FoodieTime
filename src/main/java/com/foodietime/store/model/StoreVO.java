47fda6d94dec57ca14187038cee89fd95fbd6e87
Jamie Wu <meigo1229@gmail.com>
2025-06-03 15:03:24 +0800

    fix StoreVO add JPA annotation

--- src/main/java/com/foodietime/store/model/StoreVO.java
+++ src/main/java/com/foodietime/store/model/StoreVO.java
@@ -3,27 +3,114 @@ package com.foodietime.store.model;
 import java.io.Serializable;
 import java.sql.Time;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import com.foodietime.storeCate.model.StoreCateVO;
+
+@Entity
+@Table(name = "store")
+//@NamedQuery(name = "getAllStores", query = "from store where storId > :storId order by storId desc")
 public class StoreVO implements Serializable {
 
 	// 寫上所有欄位
-	private Integer storId; // 店家編號
-	private Integer storeCateId; // 店家分類編號
-	private String storName; // 店家名稱
-	private String storDesc; // 店家敘述
-	private String storAddr; // 店家地址
-	private Double storLon; // 地址經度
-	private Double storLat; // 地址緯度
-	private String storPhone; // 店家電話
-	private String storWeb; // 店家訂位網址
-	private Time storOnTime; // 店家開店時間
-	private Time storOffTime; // 店家關店時間
-	private String storOffDay; // 店家公休日
-	private Byte storDeliver; // 提供外送
-	private Byte storStatus; // 營業狀態
-	private byte[] storPhoto; // 店家照片
-	private Byte storReportCount; // 店家被檢舉次數
-	private Integer starNum; // 總評價數
-	private Integer reviews; // 總評價人數
+	// 1.店家編號
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "STOR_ID", updatable = false)
+	private Integer storId; 
+	
+	
+	// 2.店家分類編號
+	@ManyToOne(cascade = CascadeType.PERSIST) // 多對一，指向分類，新增store時，關聯物件一起新增
+    @JoinColumn(name = "STORE_CATE_ID", referencedColumnName = "STORE_CATE_ID") // 外鍵名稱
+	private StoreCateVO storeCateId; 
+	
+	
+	public StoreCateVO getStoreCate() {
+		return storeCateId;
+	}
+	
+	public void setStoreCate(StoreCateVO storeCateId) {
+		this.storeCateId = storeCateId;
+	}
+	
+	// 3.店家名稱
+	@Column(name = "STOR_NAME")
+	private String storName; 
+	
+	// 4.店家敘述
+	@Column(name = "STOR_DESC")
+	private String storDesc; 
+	
+	// 5.店家地址
+	@Column(name = "STOR_ADDR")
+	private String storAddr; 
+	
+	// 6.地址經度
+	@Column(name = "STOR_LONGITUDE")
+	private Double storLon; 
+	
+	// 7.地址緯度
+	@Column(name = "STOR_LATITUDE")
+	private Double storLat; 
+	
+	// 8.店家電話
+	@Column(name = "STOR_PHONE")
+	private String storPhone; 
+	
+	// 9.店家訂位網址
+	@Column(name = "STOR_WEB")
+	private String storWeb; 
+	
+	// 10.店家開店時間
+	@Column(name = "STOR_OPEN_TIME")
+	private Time storOnTime;
+	
+	// 11.店家關店時間
+	@Column(name = "STOR_CLOSE_TIME")
+	private Time storOffTime; 
+	
+	// 12.店家公休日 (0:星期日,1:星期一, 2:星期二,3:星期三,4:星期四,5:星期五,6:星期六)
+	@Column(name = "STOR_WEEKLY_OFF_DAY")
+	private String storOffDay; 
+	
+	// 13.提供外送  (1:提供,0:未提供)
+	@Column(name = "STOR_DELIVER")
+	private Byte storDeliver; 
+	
+	// 14.營業狀態   (1:營業,0:未營業)
+	@Column(name = "STOR_OPEN")
+	private Byte storOpen; 
+	
+	// 15.店家照片
+	@Column(name = "STOR_PHOTO", columnDefinition = "longblob")
+	private byte[] storPhoto; 
+	
+	// 16.店家被檢舉次數
+	@Column(name = "STOR_REPORT_COUNT")
+	private Byte storReportCount; 
+	
+	// 17.總評價數  (星星數)
+	@Column(name = "TOTAL_STAR_NUM")
+	private Integer starNum; 
+	
+	// 18.總評價人數
+	@Column(name = "TOTAL_REVIEWS")
+	private Integer reviews; 
+	
+	
+	// 19.上架狀態 (1:上架 2:未上架)
+	@Column(name = "STOR_STATUS")
+	private Byte storStatus;  
 
 	public StoreVO() {
 		super();
@@ -39,13 +126,7 @@ public class StoreVO implements Serializable {
 		this.storId = storId;
 	}
 
-	public Integer getStoreCateId() {
-		return storeCateId;
-	}
 
-	public void setStoreCateId(Integer storeCateId) {
-		this.storeCateId = storeCateId;
-	}
 
 	public String getStorName() {
 		return storName;
@@ -135,13 +216,15 @@ public class StoreVO implements Serializable {
 		this.storDeliver = storDeliver;
 	}
 
-	public Byte getStorStatus() {
-		return storStatus;
+	public Byte getStorOpen() {
+		return storOpen;
 	}
 
-	public void setStorStatus(Byte storStatus) {
-		this.storStatus = storStatus;
+	public void setStorOpen(Byte storOpen) {
+		this.storOpen = storOpen;
 	}
+
+
 
 	public byte[] getStorPhoto() {
 		return storPhoto;
@@ -173,6 +256,14 @@ public class StoreVO implements Serializable {
 
 	public void setReviews(Integer reviews) {
 		this.reviews = reviews;
+	}
+	
+	public Byte getStorStatus() {
+		return storStatus;
+	}
+
+	public void setStorStatus(Byte storStatus) {
+		this.storStatus = storStatus;
 	}
 
 }
