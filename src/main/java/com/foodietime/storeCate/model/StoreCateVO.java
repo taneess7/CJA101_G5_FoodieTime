c8d856bdc225bb5ada6059aebcdb25895f8537c2
Jamie Wu <meigo1229@gmail.com>
2025-06-03 15:02:14 +0800

    fix: StoreCateVO add JPA Annotation

--- src/main/java/com/foodietime/storeCate/model/StoreCateVO.java
+++ src/main/java/com/foodietime/storeCate/model/StoreCateVO.java
@@ -1,12 +1,46 @@
 package com.foodietime.storeCate.model;
 
 import java.io.Serializable;
+import java.util.Set;
 
+
+import com.foodietime.store.model.StoreVO;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "store_category")
 public class StoreCateVO implements Serializable {
 
 	// 寫上所有欄位
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "STORE_CATE_ID", updatable = false)
 	private Integer storCateId; // 店家分類編號.
-	private String storCat; // 類型名稱
+	
+	@OneToMany(mappedBy = "storeCateId", cascade = CascadeType.ALL)
+	@OrderBy("storId asc")
+	private Set<StoreVO> stores;
+	
+	
+	public Set<StoreVO> getStores(){
+		return stores;
+	}
+	
+	public void setStores(Set<StoreVO> stores) {
+		this.stores = stores;
+	}
+	
+	@Column(name = "STORE_CATE")
+	private String storCatName; // 類型名稱
 
 	// 取得or設置
 
@@ -23,11 +57,11 @@ public class StoreCateVO implements Serializable {
 	}
 
 	public String getStorCat() {
-		return storCat;
+		return storCatName;
 	}
 
-	public void setStorCat(String storCat) {
-		this.storCat = storCat;
+	public void setStorCat(String storCatName) {
+		this.storCatName = storCatName;
 	}
 
 }
