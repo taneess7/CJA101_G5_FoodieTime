4427ecf20567debc0090a0007186c7af5a431b3e
taneess7 <taneess7@gmail.com>
2025-06-03 22:02:32 +0800

    refactor: change CartVO.java,OrdDetVO.java,OrdersVO.java to Hibernate framework
    change CartVO.java,OrdDetVO.java,OrdersVO.java to Hibernate framework

--- src/main/java/com/foodietime/cart/model/CartVO.java
+++ src/main/java/com/foodietime/cart/model/CartVO.java
@@ -1,9 +1,12 @@
 package com.foodietime.cart.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serializable;
+
 @Entity
+@Data
 @Table(name = "shopping_cart")
 public class CartVO implements Serializable {
     //寫上所有欄位
@@ -21,35 +24,35 @@ public class CartVO implements Serializable {
     private Integer prodN;    // 商品數量
 
 
-    //取得or設置_購物車商品編號
-    public Integer getShopId() {
-        return shopId;
-    }
-    public void setShopId(Integer shopId) {
-        this.shopId = shopId;
-    }
-
-    //取得or設置_會員編號
-    public Integer getMemId() {
-        return memId;
-    }
-    public void setMemId(Integer memId) {
-        this.memId = memId;
-    }
-
-    //取得or設置商品編號
-    public Integer getProdId() {
-        return prodId;
-    }
-    public void setProdId(Integer prodId) {
-        this.prodId = prodId;
-    }
-
-    //取得or設置商品數量
-    public Integer getProdN() {
-        return prodN;
-    }
-    public void setProdN(Integer prodN) {
-        this.prodN = prodN;
-    }
+//    //取得or設置_購物車商品編號
+//    public Integer getShopId() {
+//        return shopId;
+//    }
+//    public void setShopId(Integer shopId) {
+//        this.shopId = shopId;
+//    }
+//
+//    //取得or設置_會員編號
+//    public Integer getMemId() {
+//        return memId;
+//    }
+//    public void setMemId(Integer memId) {
+//        this.memId = memId;
+//    }
+//
+//    //取得or設置商品編號
+//    public Integer getProdId() {
+//        return prodId;
+//    }
+//    public void setProdId(Integer prodId) {
+//        this.prodId = prodId;
+//    }
+//
+//    //取得or設置商品數量
+//    public Integer getProdN() {
+//        return prodN;
+//    }
+//    public void setProdN(Integer prodN) {
+//        this.prodN = prodN;
+//    }
 }
