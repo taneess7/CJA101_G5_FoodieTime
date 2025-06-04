8028db65e90333db8fcab5dca459da303a4410df
taneess7 <taneess7@gmail.com>
2025-06-04 21:38:32 +0800

    refactor: change Cart part to Spring Boot framework
    change Cart part to Spring Boot framework

--- src/main/java/com/foodietime/cart/model/CartService.java
+++ src/main/java/com/foodietime/cart/model/CartService.java
@@ -1,58 +1,67 @@
 package com.foodietime.cart.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class CartService {
 
-    private CartDAO_interface dao;
+    private final CartRepository repo;
 
-    public CartService() {
-        dao = new CartDAO();
+    @Autowired
+    public CartService(CartRepository repo) {
+        this.repo = repo;
     }
+//    private CartDAO_interface dao;
+//    public CartService() {
+//        dao = new CartDAO();
+//    }
 
     // 新增購物車商品
-    public CartVO addCart(Integer memId, Integer prodId , Integer prodN) {
+    public CartVO addCart(Integer memId, Integer prodId, Integer prodN) {
         CartVO cartVO = new CartVO();
         cartVO.setMemId(memId);
         cartVO.setProdId(prodId);
         cartVO.setProdN(prodN);
-        dao.insert(cartVO);
+        repo.save(cartVO);
         return cartVO;
     }
 
     // 修改購物車商品 - 修正：加入 shopId 參數
     public CartVO updateCart(Integer shopId, Integer memId, Integer prodId, Integer prodN) {
         CartVO cartVO = new CartVO();
-        cartVO.setShopId(shopId);  
+        cartVO.setShopId(shopId);
         cartVO.setMemId(memId);
         cartVO.setProdId(prodId);
         cartVO.setProdN(prodN);
-        dao.update(cartVO);
+        repo.save(cartVO);
         return cartVO;
     }
 
     // 刪除購物車商品
     public void deleteCart(Integer shopId) {
-        dao.delete(shopId);
+        repo.deleteById(shopId);
     }
 
     // 查詢單一購物車商品
     public CartVO getOneCart(Integer shopId) {
-        return dao.findByPrimaryKey(shopId);
+        return repo.findById(shopId).orElse(null);
     }
 
     // 查詢所有購物車商品
     public List<CartVO> getAll() {
-        return dao.getAll();
+        return repo.findAll();
     }
 
     // 查詢某會員所有購物車商品
     public List<CartVO> getByMemId(Integer memId) {
-        return dao.findByMemId(memId);
+        return repo.findByMemId(memId);
     }
 
     // 查詢某會員某商品是否已存在於購物車
     public CartVO getByMemIdAndProdId(Integer memId, Integer prodId) {
-        return dao.findByMemIdAndProdId(memId, prodId);
+        return repo.findByMemIdAndProdId(memId, prodId);
     }
 }
--- src/main/java/com/foodietime/cart/model/CartVO.java
+++ src/main/java/com/foodietime/cart/model/CartVO.java
@@ -1,6 +1,9 @@
 package com.foodietime.cart.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,13 +17,19 @@ public class CartVO implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "shop_id")
     private Integer shopId;   // 購物車商品編號
+
     @Column(name = "mem_id", nullable = false)
+    @NotNull(message="不可為空")
     private Integer memId;    // 會員編號
 
     @Column(name = "prod_id", nullable = false)
-//    @ManyToOne
+    @NotNull(message="不可為空")
     private Integer prodId;   // 商品編號
+
     @Column(name = "prod_n", nullable = false)
+    @NotNull(message="不可為空")
+    @Min(value=1, message="最小1")
+    @Max(value=99, message="最大99")
     private Integer prodN;    // 商品數量
 
 }
