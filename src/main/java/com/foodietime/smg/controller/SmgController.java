757803b3f1d897802b50e13a6a942d2afcbe1f45
Peter Chen <maybefamiler@gmail.com>
2025-06-04 21:10:11 +0800

    fix:註解
    註解內容,之後再做更改

--- src/main/java/com/foodietime/smg/controller/SmgController.java
+++ src/main/java/com/foodietime/smg/controller/SmgController.java
@@ -6,20 +6,44 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.foodietime.smg.model.SmgService;
 import com.foodietime.smg.model.SmgVO;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/smg")
 public class SmgController {
 	@Autowired
 	SmgService smgSvc;
-	
+//	@PostMapping("/login")
+//    public String login(@RequestParam String account,
+//                        @RequestParam String password,
+//                        HttpSession session,
+//                        Model model) {
+//
+//        SmgVO smg = smgSvc.login(account, password);
+//
+//        if (smg != null) {
+//            session.setAttribute("loggedInSmg", smg); // 儲存商家資訊於 session
+//            return "redirect:/smg/dashboard";
+//        } else {
+//            model.addAttribute("error", "帳號或密碼錯誤");
+//            return "smgLogin"; // 返回登入頁面
+//        }
+//    }
+//
+//    @GetMapping("/logout")
+//    public String logout(HttpSession session) {
+//        session.invalidate();
+//        return "redirect:/smg/login";
+//    }
 	
 	
 }
