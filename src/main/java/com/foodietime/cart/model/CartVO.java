e3cbc5058d8d5ab381f24441324a9db7bdaeb6cc
taneess7 <taneess7@gmail.com>
2025-06-04 14:31:08 +0800

    refactor: change CartVO.java,OrdDetVO.java,OrdersVO.java ,MemCouponVO.javato Hibernate framework
    change CartVO.java,OrdDetVO.java,OrdersVO.java ,MemCouponVO.javato Hibernate framework

--- src/main/java/com/foodietime/cart/model/CartVO.java
+++ src/main/java/com/foodietime/cart/model/CartVO.java
@@ -23,36 +23,4 @@ public class CartVO implements Serializable {
     @Column(name = "prod_n", nullable = false)
     private Integer prodN;    // 商品數量
 
-
-//    //取得or設置_購物車商品編號
-//    public Integer getShopId() {
-//        return shopId;
-//    }
-//    public void setShopId(Integer shopId) {
-//        this.shopId = shopId;
-//    }
-//
-//    //取得or設置_會員編號
-//    public Integer getMemId() {
-//        return memId;
-//    }
-//    public void setMemId(Integer memId) {
-//        this.memId = memId;
-//    }
-//
-//    //取得or設置商品編號
-//    public Integer getProdId() {
-//        return prodId;
-//    }
-//    public void setProdId(Integer prodId) {
-//        this.prodId = prodId;
-//    }
-//
-//    //取得or設置商品數量
-//    public Integer getProdN() {
-//        return prodN;
-//    }
-//    public void setProdN(Integer prodN) {
-//        this.prodN = prodN;
-//    }
 }
