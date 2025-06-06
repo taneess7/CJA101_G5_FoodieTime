680abaa2e1af02411fc4d36553a83fc877f77aae
chan <h1257465876@gmail.com>
2025-06-06 21:12:00 +0800

    Feat : 新增post.model

---
+++ src/main/java/com/foodietime/post/model/PostDAO_interface.java
@@ -0,0 +1,15 @@
+package com.foodietime.post.model;
+
+import java.util.List;
+
+public interface PostDAO_interface {
+	
+	public void insert(PostVO postVO);
+	public void update(PostVO postVO);
+	public void delete(Integer postId);
+	
+	public PostVO findByPK(Integer postId);
+	public List<PostVO> getAll();
+	
+
+}
