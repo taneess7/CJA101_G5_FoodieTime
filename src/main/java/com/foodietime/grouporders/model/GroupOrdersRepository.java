adff7a17958489b01639104d8ac7f462d5da07f8
ikth <karen61020@gmail.com>
2025-06-10 11:48:48 +0800

    chore：新增GroupOrdersRepository、GroupBuyingCasesVO

---
+++ src/main/java/com/foodietime/grouporders/model/GroupOrdersRepository.java
@@ -0,0 +1,9 @@
+package com.foodietime.grouporders.model;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupOrdersRepository extends JpaRepository<GroupOrdersVO, Integer>{
+
+	
+}
