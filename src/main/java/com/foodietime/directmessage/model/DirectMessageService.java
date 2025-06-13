d39aee73afbf04571c83d65391924dc8b3a99b81
sun <max8902181@hotmail.com>
2025-06-13 18:07:32 +0800

    test:repo跟service註解測試

--- src/main/java/com/foodietime/directmessage/model/DirectMessageService.java
+++ src/main/java/com/foodietime/directmessage/model/DirectMessageService.java
@@ -1,43 +1,43 @@
-package com.foodietime.directmessage.model;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.foodietime.member.model.MemberVO;
-import com.foodietime.smg.model.SmgVO;
-
-
-@Service
-public class DirectMessageService {
-	 @Autowired
-	    private DirectMessageRepository messageRepo;
-
-	    // 新增訊息
-	    public DirectMessageVO addMessage(DirectMessageVO message) {
-	        message.setMessTime(LocalDateTime.now());
-	        return messageRepo.save(message);
-	    }
-
-	    // 查詢會員的訊息
-	    public List<DirectMessageVO> getMessagesByMemberId(MemberVO member) {
-	        return messageRepo.findByMember_MemIdOrderByMessTimeAsc(member);
-	    }
-
-	    // 查詢管理員的訊息
-	    public List<DirectMessageVO> getMessagesBySmgrId(SmgVO smgr) {
-	        return messageRepo.findBySmgrId_SmgrIdOrderByMessTimeAsc(smgr);
-	    }
-
-	    // 刪除訊息
-	    public void deleteMessage(Integer dmId) {
-	        messageRepo.deleteById(dmId);
-	    }
-	    
-	 // 取得單筆訊息 by dmId
-	    public DirectMessageVO getById(Integer dmId) {
-	        return messageRepo.findById(dmId).orElse(null);
-	    }
-}
+//package com.foodietime.directmessage.model;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.foodietime.member.model.MemberVO;
+//import com.foodietime.smg.model.SmgVO;
+//
+//
+//@Service
+//public class DirectMessageService {
+//	 @Autowired
+//	    private DirectMessageRepository messageRepo;
+//
+//	    // 新增訊息
+//	    public DirectMessageVO addMessage(DirectMessageVO message) {
+//	        message.setMessTime(LocalDateTime.now());
+//	        return messageRepo.save(message);
+//	    }
+//
+//	    // 查詢會員的訊息
+//	    public List<DirectMessageVO> getMessagesByMemberId(Integer memId) {
+//	        return messageRepo.findByMember_MemIdOrderByMessTimeAsc(memId);
+//	    }
+//
+//	    // 查詢管理員的訊息
+//	    public List<DirectMessageVO> getMessagesBySmgrId(Integer smgrId) {
+//	        return messageRepo.findBySmgrId_SmgrIdOrderByMessTimeAsc(smgrId);
+//	    }
+//
+//	    // 刪除訊息
+//	    public void deleteMessage(Integer dmId) {
+//	        messageRepo.deleteById(dmId);
+//	    }
+//	    
+//	 // 取得單筆訊息 by dmId
+//	    public DirectMessageVO getById(Integer dmId) {
+//	        return messageRepo.findById(dmId).orElse(null);
+//	    }
+//}
--- src/main/java/com/foodietime/member/model/MemService.java
+++ src/main/java/com/foodietime/member/model/MemService.java
@@ -1,69 +1,69 @@
-package com.foodietime.member.model;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MemService {
-	@Autowired
-    private MemberRepository memberRepository;
-
-    // 新增或修改會員
-    public MemberVO save(MemberVO memberVO) {
-        return memberRepository.save(memberVO);
-    }
-
-    // 查詢所有會員
-    public List<MemberVO> getAll() {
-        return memberRepository.findAll();
-    }
-
-    // 根據 ID 查詢
-    public MemberVO getById(Integer memId) {
-        return memberRepository.findById(memId).orElse(null);
-    }
-
-    // 根據帳號查詢
-    public MemberVO getByAccount(String memAccount) {
-        return memberRepository.findByMemAccount(memAccount);
-    }
-
-    // 檢查帳號是否存在
-    public boolean isAccountExist(String memAccount) {
-        return memberRepository.existsByMemAccount(memAccount);
-    }
-
-    // 刪除會員
-    public void delete(Integer memId) {
-        memberRepository.deleteById(memId);
-    }
-
-    // 更新頭像
-    public void updateAvatar(Integer memId, byte[] avatar) {
-        Optional<MemberVO> optional = memberRepository.findById(memId);
-        if (optional.isPresent()) {
-            MemberVO member = optional.get();
-            member.setMemAvatar(avatar);
-            memberRepository.save(member); // 自動 merge
-        }
-    }
-    public MemberVO login(String memAccount, String memPassword) {
-        MemberVO member = memberRepository.findByMemAccount(memAccount);
-        if (member != null && member.getMemPassword().equals(memPassword)) {
-            return member;
-        }
-        return null;
-    }
-    
-    public boolean isAccountExists(String memAccount) {
-        return memberRepository.existsByMemAccount(memAccount);
-    }
-
-    public boolean isEmailExists(String memEmail) {
-        return memberRepository.existsByMemEmail(memEmail);
-    }
-
-}
+//package com.foodietime.member.model;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MemService {
+//	@Autowired
+//    private MemberRepository memberRepository;
+//
+//    // 新增或修改會員
+//    public MemberVO save(MemberVO memberVO) {
+//        return memberRepository.save(memberVO);
+//    }
+//
+//    // 查詢所有會員
+//    public List<MemberVO> getAll() {
+//        return memberRepository.findAll();
+//    }
+//
+//    // 根據 ID 查詢
+//    public MemberVO getById(Integer memId) {
+//        return memberRepository.findById(memId).orElse(null);
+//    }
+//
+//    // 根據帳號查詢
+//    public MemberVO getByAccount(String memAccount) {
+//        return memberRepository.findByMemAccount(memAccount);
+//    }
+//
+//    // 檢查帳號是否存在
+//    public boolean isAccountExist(String memAccount) {
+//        return memberRepository.existsByMemAccount(memAccount);
+//    }
+//
+//    // 刪除會員
+//    public void delete(Integer memId) {
+//        memberRepository.deleteById(memId);
+//    }
+//
+//    // 更新頭像
+//    public void updateAvatar(Integer memId, byte[] avatar) {
+//        Optional<MemberVO> optional = memberRepository.findById(memId);
+//        if (optional.isPresent()) {
+//            MemberVO member = optional.get();
+//            member.setMemAvatar(avatar);
+//            memberRepository.save(member); // 自動 merge
+//        }
+//    }
+//    public MemberVO login(String memAccount, String memPassword) {
+//        MemberVO member = memberRepository.findByMemAccount(memAccount);
+//        if (member != null && member.getMemPassword().equals(memPassword)) {
+//            return member;
+//        }
+//        return null;
+//    }
+//    
+//    public boolean isAccountExists(String memAccount) {
+//        return memberRepository.existsByMemAccount(memAccount);
+//    }
+//
+//    public boolean isEmailExists(String memEmail) {
+//        return memberRepository.existsByMemEmail(memEmail);
+//    }
+//
+//}
