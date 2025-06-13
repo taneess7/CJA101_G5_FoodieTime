018477148827038225067ac999b3ee640f5ff7c7
Hikari <wr232688@gmail.com>
2025-06-13 18:47:04 +0800

    test

--- src/main/java/com/foodietime/memfavlist/model/FavoriteListService.java
+++ src/main/java/com/foodietime/memfavlist/model/FavoriteListService.java
@@ -1,53 +1,53 @@
-package com.foodietime.memfavlist.model;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class FavoriteListService {
-
-	private final FavoriteListRepository fvlistrepo;
-	
-	@Autowired
-	public FavoriteListService(FavoriteListRepository fvlistrepo) {
-		this.fvlistrepo = fvlistrepo;
-	}
-	
-	//新增收藏
-	public FavoriteListVO addFavoriteList(Integer memId,Integer prodId) {
-		FavoriteListVO favoriteListVO = new FavoriteListVO();
-		
-		favoriteListVO.setMemId(memId);
-		favoriteListVO.setProdId(prodId);
-		fvlistrepo.save(favoriteListVO);
-		
-		return favoriteListVO;
-	}
-	
-	//刪除單一收藏
-	public void deleteFavorite(Integer memId, Integer prodId) {
-		fvlistrepo.deleteByMemIdAndProdId(memId, prodId);
-	}
-	//刪除某會員所有收藏
-	public void deleteAllFavoritesByMem(Integer memId) {
-		fvlistrepo.deleteAllByMemId(memId);
-	}
-
-	//查詢單一收藏
-	public FavoriteListVO getOneFavorite(Integer memId,Integer prodId) {
-		return fvlistrepo.findByMemIdAndProdId(memId, prodId);
-	}
-	
-	//查詢所有收藏
-	public List<FavoriteListVO> getAll(){
-		return fvlistrepo.findAll();
-	}
-	
-	//查詢某會員所有收藏
-	public List<FavoriteListVO> getFavoritesByMemId(Integer memId) {
-		return fvlistrepo.findAllByMemId(memId);
-	}
-}
+//package com.foodietime.memfavlist.model;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class FavoriteListService {
+//
+//	private final FavoriteListRepository fvlistrepo;
+//	
+//	@Autowired
+//	public FavoriteListService(FavoriteListRepository fvlistrepo) {
+//		this.fvlistrepo = fvlistrepo;
+//	}
+//	
+//	//新增收藏
+//	public FavoriteListVO addFavoriteList(Integer memId,Integer prodId) {
+//		FavoriteListVO favoriteListVO = new FavoriteListVO();
+//		
+//		favoriteListVO.setMemId(memId);
+//		favoriteListVO.setProdId(prodId);
+//		fvlistrepo.save(favoriteListVO);
+//		
+//		return favoriteListVO;
+//	}
+//	
+//	//刪除單一收藏
+//	public void deleteFavorite(Integer memId, Integer prodId) {
+//		fvlistrepo.deleteByMemIdAndProdId(memId, prodId);
+//	}
+//	//刪除某會員所有收藏
+//	public void deleteAllFavoritesByMem(Integer memId) {
+//		fvlistrepo.deleteAllByMemId(memId);
+//	}
+//
+//	//查詢單一收藏
+//	public FavoriteListVO getOneFavorite(Integer memId,Integer prodId) {
+//		return fvlistrepo.findByMemIdAndProdId(memId, prodId);
+//	}
+//	
+//	//查詢所有收藏
+//	public List<FavoriteListVO> getAll(){
+//		return fvlistrepo.findAll();
+//	}
+//	
+//	//查詢某會員所有收藏
+//	public List<FavoriteListVO> getFavoritesByMemId(Integer memId) {
+//		return fvlistrepo.findAllByMemId(memId);
+//	}
+//}
