734941a03fda09f5180afcb3039889fe2525b0ef
Hikari <wr232688@gmail.com>
2025-06-13 18:47:04 +0800

    test

--- src/main/java/com/foodietime/memfavlist/model/FavoriteListRepository.java
+++ src/main/java/com/foodietime/memfavlist/model/FavoriteListRepository.java
@@ -1,21 +1,21 @@
-package com.foodietime.memfavlist.model;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteListRepository extends JpaRepository<FavoriteListVO, FavoriteListId>{
-	
-	//根據複合主鍵查詢
-	FavoriteListVO findByMemIdAndProdId(Integer memId, Integer prodId);
-
-    // 刪除單一收藏
-    void deleteByMemIdAndProdId(Integer memId, Integer prodId);
-
-    // 刪除某會員的所有收藏
-    void deleteAllByMemId(Integer memId);
-
-    // 查詢某會員所有收藏
-    List<FavoriteListVO> findAllByMemId(Integer memId);
-
-}
+//package com.foodietime.memfavlist.model;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface FavoriteListRepository extends JpaRepository<FavoriteListVO, FavoriteListId>{
+//	
+//	//根據複合主鍵查詢
+//	FavoriteListVO findByMemIdAndProdId(Integer memId, Integer prodId);
+//
+//    // 刪除單一收藏
+//    void deleteByMemIdAndProdId(Integer memId, Integer prodId);
+//
+//    // 刪除某會員的所有收藏
+//    void deleteAllByMemId(Integer memId);
+//
+//    // 查詢某會員所有收藏
+//    List<FavoriteListVO> findAllByMemId(Integer memId);
+//
+//}
