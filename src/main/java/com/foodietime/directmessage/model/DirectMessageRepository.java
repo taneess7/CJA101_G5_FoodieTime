abb2aaef7950c1627fc3f9bade053b13fbfcef0b
sun <max8902181@hotmail.com>
2025-06-15 00:42:47 +0800

    fix:修正repo

--- src/main/java/com/foodietime/directmessage/model/DirectMessageRepository.java
+++ src/main/java/com/foodietime/directmessage/model/DirectMessageRepository.java
@@ -1,16 +1,16 @@
-//package com.foodietime.directmessage.model;
-//
-//import java.util.List;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.foodietime.member.model.MemberVO;
-//import com.foodietime.smg.model.SmgVO;
-//
-//public interface DirectMessageRepository extends JpaRepository<DirectMessageVO, Integer>{
-//	 // 依會員查詢所有訊息（可排序）
-//    List<DirectMessageVO> findByMember_MemIdOrderByMessTimeAsc(MemberVO member);
-//
-//    // 依管理者查詢所有訊息（可排序）
-//    List<DirectMessageVO> findBySmgrId_SmgrIdOrderByMessTimeAsc(SmgVO smgr);
-//}
+package com.foodietime.directmessage.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.foodietime.smg.model.SmgVO;
+
+public interface DirectMessageRepository extends JpaRepository<DirectMessageVO, Integer>{
+	 // 依會員查詢所有訊息（可排序）
+    List<DirectMessageVO> findByMember_MemIdOrderByMessTimeAsc(Integer memId);
+
+    // 依管理者查詢所有訊息（可排序）
+    List<DirectMessageVO> findBySmgrOrderByMessTimeAsc(SmgVO smgr);
+
+    }
