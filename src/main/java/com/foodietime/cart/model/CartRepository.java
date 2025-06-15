cd908cc25328f9fa4d7f84791450df59b61d2d5b
taneess7 <taneess7@gmail.com>
2025-06-15 23:05:18 +0800

    modify: add new Cart
    add new Cart

--- src/main/java/com/foodietime/cart/model/CartRepository.java
+++ src/main/java/com/foodietime/cart/model/CartRepository.java
@@ -1,14 +1,16 @@
-//package com.foodietime.cart.model;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface CartRepository extends JpaRepository<CartVO, Integer> {
-//    // 查詢某會員的所有購物車商品
-//    List<CartVO> findByMemId(Integer memId);
-//
-//    // 查詢某會員某商品是否已存在於購物車
-//    CartVO findByMemIdAndProdId(Integer memId, Integer prodId);
-//
-//}
+package com.foodietime.cart.model;
+
+import com.foodietime.member.model.MemberVO;
+import com.foodietime.product.model.ProductVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<CartVO, Integer> {
+    // 查詢某會員的所有購物車商品
+    List<CartVO> findByMember(MemberVO member);
+
+    // 查詢某會員某商品是否已存在於購物車
+    CartVO findByMemberAndProduct(MemberVO member, ProductVO product);
+
+}
