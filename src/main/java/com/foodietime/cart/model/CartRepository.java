9cfbf7f3baf5a67fe57216a31d043c456ec1f1ff
taneess7 <taneess7@gmail.com>
2025-06-17 15:16:12 +0800

    modify: add method to CartService.java
    add method to CartService.java

--- src/main/java/com/foodietime/cart/model/CartRepository.java
+++ src/main/java/com/foodietime/cart/model/CartRepository.java
@@ -5,6 +5,7 @@ import com.foodietime.product.model.ProductVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<CartVO, Integer> {
     // 查詢某會員的所有購物車商品
@@ -13,4 +14,6 @@ public interface CartRepository extends JpaRepository<CartVO, Integer> {
     // 查詢某會員某商品是否已存在於購物車
     CartVO findByMemberAndProduct(MemberVO member, ProductVO product);
 
+    // 查詢某會員的某特定購物車編號，確保存取權限
+    Optional<CartVO> findByShopIdAndMember(Integer shopId, MemberVO member);
 }
