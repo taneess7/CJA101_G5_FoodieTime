7557311fc997ff868ebd8b87cc42991aa1a07b41
ikth <karen61020@gmail.com>
2025-06-26 19:33:27 +0800

    fix：修改leaderindex網頁

--- src/main/java/com/foodietime/gbprod/controller/gbleaderservlet.java
+++ src/main/java/com/foodietime/gbprod/controller/gbleaderservlet.java
@@ -1,5 +1,40 @@
 package com.foodietime.gbprod.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.foodietime.gbprod.model.GbprodVO;
+import com.foodietime.gbprod.service.GbleaderService;
+
+@Controller
+@RequestMapping("/gb")
 public class gbleaderservlet {
 
+	 @Autowired
+	 private GbleaderService gbleaderService;
+    
+	 	/**
+	     * 查詢店家的促銷價格
+	     * @param storeName 店家名稱
+	     * @param model 用來傳遞資料到前端
+	     * @return 返回顯示頁面
+	     */
+	    @GetMapping("/leaderindex")
+	    public String getStoresWithPromotionPrices(Model model) {
+	        // 呼叫 Service 層獲取指定店家的促銷價格
+	        List<GbprodVO> storesWithPromotion = gbleaderService.getStoresWithPromotionPrices();
+
+	        // 把結果添加到模型中
+	        model.addAttribute("storesWithPromotion", storesWithPromotion);
+
+	        // 返回要顯示頁面
+	        return "front/gb/gbleader/leaderindex";  
+	    }
+        
 }
--- src/main/java/com/foodietime/gbprod/model/GbprodRepository.java
+++ src/main/java/com/foodietime/gbprod/model/GbprodRepository.java
@@ -1,10 +1,24 @@
 package com.foodietime.gbprod.model;
 
 import com.foodietime.smg.model.SmgVO;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GbprodRepository extends JpaRepository<GbprodVO, Integer> {
+
+	
+	@Query("""
+		      SELECT p 
+		      FROM GbprodVO p
+		      JOIN p.groupOrdersList go
+		      WHERE p.gbProdStatus = 1
+		      """)
+		List<GbprodVO> findStoresWithOrdersByStatus();
 
 }
