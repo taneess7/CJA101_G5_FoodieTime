a37ba1941f2a2b04a1224607f3d5943082610e64
Peter Chen <maybefamiler@gmail.com>
2025-07-01 09:28:25 +0800

    feat:團購檢舉清單
    新增檢舉清單檢視

--- src/main/java/com/foodietime/config/WebSocketConfig.java
+++ src/main/java/com/foodietime/config/WebSocketConfig.java
@@ -1,21 +1,22 @@
-
-package com.foodietime.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-/**
- * WebSocket 組態類。
- * 這個 Bean 會自動註冊所有使用 @ServerEndpoint 註解宣告的 WebSocket endpoint。
- */
-@Configuration
-@EnableAsync
-public class WebSocketConfig {
-
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
-
+//
+//package com.foodietime.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+//
+///**
+// * WebSocket 組態類。
+// * 這個 Bean 會自動註冊所有使用 @ServerEndpoint 註解宣告的 WebSocket endpoint。
+// */
+//@Configuration
+//@EnableAsync
+//public class WebSocketConfig {
+//
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
+//
+//}
